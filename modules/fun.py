b56215974da594bda7f4df8d57cb3685f796d45d
Lapoozza <jewelmahanta@gmail.com>
2017-05-21 11:26:56 +0530

    Added a proper module system

---
+++ modules/fun.py
@@ -0,0 +1,14 @@
+from assistant import command
+
+
+class UtilityCommands:
+    def __init__(self, assistant):
+        self.assistant = assistant
+
+    @command(description="A sample command to demonstrate modules.")
+    async def test_state(self, message):
+        await self.assistant.send_message(message.channel, "This is from a downloaded module.")
+
+
+def load(assistant):
+    assistant.add_module(UtilityCommands(assistant))
